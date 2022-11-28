e9f54e817f4431d0a88fecf769e1450166fff41d
Daniel Correa <fernandocorrea256@gmail.com>
2022-11-28 09:16:27 -0500

    better interface for producer & consumer

--- consumer/index.js
+++ consumer/index.js
@@ -1,38 +1,33 @@
 const http = require('http');
-const fs = require('fs');
-const url = require('url');
 const { Kafka } = require('kafkajs')
+const broker = process.env.broker || 'localhost:9092'
 
 const kafka = new Kafka({
     clientId: 'my-app',
-    brokers: ['localhost:9092'],
+    brokers: [broker],
  })
 
 const consumer = kafka.consumer({ groupId: 'test-group' }) 
+const messages = []
 
 async function connectConsumer() {
     await consumer.connect()
     await consumer.subscribe({ topic: 'test-topic', fromBeginning: false })
     await consumer.run({
         eachMessage: async ({ topic, partition, message }) => {
-            console.log(message)
+            let object = {partition, message}
+            messages.push(object)
+            console.log(object.message.value.toString())
         },
     })
 }
 
-fs.readFile('./index.html', async function (err, html) {
+connectConsumer()
 
-    await connectConsumer()
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end(messages.reduce((acumulator, current) => acumulator + `${current.partition}:${current.message.value.toString()}\n`, ""))
+}
 
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(async function(req, res) {  
-        const urlRequest = new URL(req.url, `http://${req.headers.host}`)
-        res.write("he")
-        console.log("xd")
-        res.write("llo")
-        res.end();  
-    
-    }).listen(8081, () => console.log("running consumer..."));
-});
+const server = http.createServer(requestListener);
+server.listen(8081, () => console.log("consumer running..."));
